ac59b9d507a9b4ed8dc6e17946cd9e91fac3edc3
Gotkwondo <dhwngus99@gmail.com>
2022-01-28 00:14:15 +0900

    Day4 (rebuilding)

--- FirstWeb/JS/sideBar.js
+++ FirstWeb/JS/sideBar.js
@@ -1,16 +1,22 @@
 const op_bt = document.querySelector("#sidebar");
 const nav_bar = document.querySelector("#nav");
 const close_bt = document.querySelector("#nav_bt_close");
+const sideBarWidt = document.qu
 
 //sidebar의 동작을 담당하는 함수
 function active_sideBar() {
-  op_bt.addEventListener("click", op_nav);
-  close_bt.addEventListener("click", cl_nav);
+  // op_bt.addEventListener("click", op_nav);
+  // close_bt.addEventListener("click", cl_nav);
+  console.log(sidebarWidth);
+  
 }
 
 function op_nav() {
+  console.log(sidebarWidth);
   op_bt.setAttribute("aria-pressed", "true");
-  nav_bar.classList.add("active");
+  nav_bar.style.width = `${sidebarWidth}px`;
+  document.querySelector("#body").style.marginLeft = `-${sidebarWidth}px`;
+  
 }
 
 function cl_nav() {
