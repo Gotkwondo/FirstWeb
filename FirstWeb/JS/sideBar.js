efff455e1403600f9f6f2499e456e8d6d36cd5c5
Gotkwondo <dhwngus99@gmail.com>
2022-01-28 00:38:13 +0900

    Day4 recap

--- FirstWeb/JS/sideBar.js
+++ FirstWeb/JS/sideBar.js
@@ -1,21 +1,21 @@
 const op_bt = document.querySelector("#sidebar");
 const nav_bar = document.querySelector("#nav");
 const close_bt = document.querySelector("#nav_bt_close");
-const sideBarWidt = document.qu
+const sideBarWidth = document.querySelector("#body")
 
 //sidebar의 동작을 담당하는 함수
 function active_sideBar() {
   // op_bt.addEventListener("click", op_nav);
   // close_bt.addEventListener("click", cl_nav);
-  console.log(sidebarWidth);
+  console.log(document.body.marginLeft);
   
 }
 
 function op_nav() {
   console.log(sidebarWidth);
   op_bt.setAttribute("aria-pressed", "true");
-  nav_bar.style.width = `${sidebarWidth}px`;
-  document.querySelector("#body").style.marginLeft = `-${sidebarWidth}px`;
+  nav_bar.style.width = `${sideBarWidth}px`;
+  document.querySelector("#body").style.marginLeft = `-${sideBarWidth}px`;
   
 }
 
