e2b7caddb2f58daf89c1df272ce430a2ccdf13b1
Gotkwondo <dhwngus99@gmail.com>
2022-01-31 17:09:05 +0900

    day6

--- FirstWeb/JS/sideBar.js
+++ FirstWeb/JS/sideBar.js
@@ -1,25 +1,25 @@
-const op_bt = document.querySelector("#sidebar");
-const nav_bar = document.querySelector("#nav");
-const close_bt = document.querySelector("#nav_bt_close");
+// const op_bt = document.querySelector("#sidebar");
+// const nav_bar = document.querySelector("#nav");
+// const close_bt = document.querySelector("#nav_bt_close");
 
-//sidebar의 동작을 담당하는 함수
-function active_sideBar() {
-  op_bt.addEventListener("click", op_nav);
-  // close_bt.addEventListener("click", cl_nav);
+// //sidebar의 동작을 담당하는 함수
+// function active_sideBar() {
+//   op_bt.addEventListener("click", op_nav);
+//   // close_bt.addEventListener("click", cl_nav);
   
-}
+// }
 
-function op_nav() {
-  op_bt.setAttribute("aria-pressed", "true");
-  nav_bar.style.width = "100px";
+// function op_nav() {
+//   op_bt.setAttribute("aria-pressed", "true");
+//   nav_bar.style.width = "100px";
   
   
-}
+// }
 
-function cl_nav() {
-  op_bt.setAttribute("aria-pressed", "false");
-  nav_bar.classList.remove("active");
-}
+// function cl_nav() {
+//   op_bt.setAttribute("aria-pressed", "false");
+//   nav_bar.classList.remove("active");
+// }
 
 
-active_sideBar();
+// active_sideBar();
